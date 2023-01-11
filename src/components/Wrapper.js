024e9af0e952200490b1ef93f66a1048672141b0
Mika Bibas <mikabibas@gmail.com>
2023-01-11 20:29:55 +0200

    new calculator project

---
+++ src/components/Wrapper.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Wrapper.module.css";
+
+function wrapper() {
+  return <div className={styles.wrapper}>Hello Shir</div>;
+}
+
+export default wrapper;
